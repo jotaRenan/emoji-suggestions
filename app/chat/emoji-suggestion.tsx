9df78121068776bf127ffb53d599eb519bd7c72b
Joao Pedro Renan <jota.renan@gmail.com>
2024-05-25 15:15:27 -0300

    search improvement

--- app/chat/emoji-suggestion.tsx
+++ app/chat/emoji-suggestion.tsx
@@ -1,5 +1,8 @@
 import { CompactEmoji } from "emojibase";
 import compactEmojis from "emojibase-data/pt/compact.json";
+import { useState } from "react";
+
+const initial: ReadonlyArray<CompactEmoji> = [];
 
 export const EmojiSuggestion = ({
   onChange,
@@ -8,18 +11,34 @@ export const EmojiSuggestion = ({
   searchTerm: string;
   onChange: (emoji: CompactEmoji) => void;
 }) => {
+  const [lastSuccessfulMatch, setLastSuccessfulMatch] = useState(initial);
+
+  if (searchTerm.length <= 2) {
+    if (lastSuccessfulMatch !== initial) {
+      setLastSuccessfulMatch(initial);
+    }
+    return null;
+  }
+
+  const matches = compactEmojis.filter(
+    (e) => e.tags?.some((tag) => tag.startsWith(searchTerm)) ?? false
+  );
+
+  if (matches.length > 0 && lastSuccessfulMatch.length !== matches.length) {
+    setLastSuccessfulMatch(matches);
+  }
+
   return (
     <div className="positioned-notice emoji-suggestion-box">
-      {searchTerm.length > 2 &&
-        compactEmojis
-          .filter(
-            (e) => e.tags?.some((tag) => tag.startsWith(searchTerm)) ?? false
-          )
-          .map((e) => (
-            <button key={e.unicode} onClick={() => onChange(e)}>
-              {e.unicode}
-            </button>
-          ))}
+      {lastSuccessfulMatch.length === 0 ? (
+        <span>None found</span>
+      ) : (
+        lastSuccessfulMatch.map((e) => (
+          <div key={e.unicode}>
+            <button onClick={() => onChange(e)}>{e.unicode}</button>
+          </div>
+        ))
+      )}
     </div>
   );
 };
--- app/chat/textarea.tsx
+++ app/chat/textarea.tsx
@@ -10,6 +10,7 @@ export const TextArea = () => {
   const [text, setText] = useState("");
 
   const textAreaRef = useRef<ElementRef<"textarea">>(null);
+
   function reset() {
     setIsOn(false);
     setSearchTerm("");
@@ -53,32 +54,26 @@ export const TextArea = () => {
         value={text}
         rows={3}
         onChange={(e) => setText(e.currentTarget.value)}
+        onInput={(e) => {
+          const lastEntireWord = getLastEntireWord(e);
+          if (!isOn) {
+            return false;
+          }
+
+          if (lastEntireWord) {
+            setSearchTerm(lastEntireWord.slice(1));
+          } else {
+            reset();
+          }
+        }}
         onKeyDown={(e) => {
-          const currentValue = e.currentTarget.value;
-
-          const currentPosition = e.currentTarget.selectionStart;
-          const lastEntireWord = currentValue
-            .slice(0, currentPosition)
-            .split(" ")
-            .at(-1);
-
           if (e.key !== LIMIT_CHAR) {
             if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey) {
               return false;
             }
-
-            if (isOn) {
-              if (/[\n\r\s]+/.test(e.key) || e.key === "Enter") {
-                reset();
-              } else {
-                if (lastEntireWord) {
-                  setSearchTerm(lastEntireWord.slice(1));
-                }
-              }
-            }
             return false;
           }
-
+          const lastEntireWord = getLastEntireWord(e);
           if (lastEntireWord === undefined) {
             return false;
           }
@@ -109,18 +104,31 @@ export const TextArea = () => {
           }
         }}
       />
-      {isOn && (
-        <EmojiSuggestion
-          searchTerm={searchTerm}
-          onChange={(e) => {
-            addEmoji(e.unicode);
-            reset();
-            textAreaRef.current?.focus();
-          }}
-        />
-      )}
+      <EmojiSuggestion
+        searchTerm={searchTerm}
+        onChange={(e) => {
+          addEmoji(e.unicode);
+          reset();
+          textAreaRef.current?.focus();
+        }}
+      />
       <p>{text}</p>
       {JSON.stringify(isOn, null, 2)}
     </>
   );
 };
+
+function getLastEntireWord(
+  e:
+    | React.FormEvent<HTMLTextAreaElement>
+    | React.KeyboardEvent<HTMLTextAreaElement>
+) {
+  const currentValue = e.currentTarget.value;
+
+  const currentPosition = e.currentTarget.selectionStart;
+  const lastEntireWord = currentValue
+    .slice(0, currentPosition)
+    .split(" ")
+    .at(-1);
+  return lastEntireWord;
+}
